32456c6c34a855b7fc0d97d7d60152a96872aa87
Murashko Vladislav <vladislavuh2000@gmail.com>
2025-07-18 19:39:49 +0300

    gap added

--- client/src/components/carousel_list/TrackSmallCarouselList.tsx
+++ client/src/components/carousel_list/TrackSmallCarouselList.tsx
@@ -10,7 +10,7 @@ interface TracklistListProps {
 
 const TrackSmallCarouselList: FC<TracklistListProps> = ({tracks, itemWidth, name}) => {
     return (
-        <ListCarousel title={name} font_size_title={24} count_items={tracks.length}>
+        <ListCarousel title={name} font_size_title={24} gap={24} count_items={tracks.length}>
             {tracks.map((track, index) => (
                 <PublicPlaylistItem key={index} playlist={track} itemWidth={itemWidth} />
             ))}

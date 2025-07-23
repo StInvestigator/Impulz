7152730f6052fb539ea55eb8890585a56f5d742f
KochergaKostia228 <145696471+KochergaKostia228@users.noreply.github.com>
2025-07-23 11:51:33 +0300

    Add language

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -58,6 +58,7 @@ const Navbar = memo(() => {
                         <Typography variant={"mainSbL"} textTransform={"none"}>
                             {t("login")}
                         </Typography>
+
                     </Button>
                 </Box>
             </Toolbar>

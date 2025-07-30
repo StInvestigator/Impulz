12c1b5c089525d263733962553e7d9a991ea041b
Murashko Vladislav <vladislavuh2000@gmail.com>
2025-07-30 14:17:08 +0300

    models + mock objects added

--- server/src/main/java/com/example/server/model/User.java
+++ server/src/main/java/com/example/server/model/User.java
@@ -1,18 +1,47 @@
 package com.example.server.model;
 
+import com.example.server.model.id.UserFavoriteAlbum;
+import com.example.server.model.id.UserFavoritePlaylist;
+import jakarta.persistence.*;
 import lombok.Data;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
+@Entity
+@Table(name = "users")
 public class User {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+    @Column(nullable = false, unique = true, length = 100)
     private String email;
-    private String password_hash;
-    private Role role;
-    private List<Playlist> playlists = new ArrayList<>();
-    private LocalDate endPremium = null;
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+    @Column(name = "image_url")
+    private String imageUrl;
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
 
+    @OneToMany(mappedBy = "owner")
+    private Set<Playlist> playlists = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "user")
+    private Set<UserFavoriteAlbum> favoriteAlbums = new HashSet<>();
+    @OneToMany(mappedBy = "user")
+    private Set<UserFavoritePlaylist> favoritePlaylists = new HashSet<>();
+    @OneToOne(mappedBy = "user")
+    private Author authorProfile;
 }

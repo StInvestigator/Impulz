b2c276030eff9dd46715ee791f47998b6a7ecbb3
Murashko Vladislav <vladislavuh2000@gmail.com>
2025-08-06 21:24:47 +0300

    quick fix

--- client/src/keycloak.ts
+++ client/src/keycloak.ts
@@ -3,7 +3,7 @@ import Keycloak from "keycloak-js";
 // @ts-ignore
 const keycloak = new Keycloak({
     url: "http://localhost:9090",
-    realm: "ImpulzRealm",
+    realm: "Impulz",
     clientId: "ImpulzClient"
 });
 

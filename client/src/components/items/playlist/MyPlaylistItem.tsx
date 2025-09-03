c6a6ce30c7cc9711cdb94388c94a299d34139509
KochergaKostia228 <145696471+KochergaKostia228@users.noreply.github.com>
2025-09-03 16:11:21 +0300

    hover fix

--- client/src/components/items/playlist/MyPlaylistItem.tsx
+++ client/src/components/items/playlist/MyPlaylistItem.tsx
@@ -1,8 +1,6 @@
-import type {FC} from "react";
+import {useState, type FC} from "react";
 import { ListItem, ListItemButton, Box, Typography, ListItemIcon, ListItemText } from "@mui/material";
-import pushPinImage from "../../../assets/pushPin.svg"
 import { useTranslation } from 'react-i18next';
-
 interface PlaylistProps {
     image: string,
     name: string,
@@ -10,34 +8,51 @@ interface PlaylistProps {
 }
 
 const MyPlaylistItem: FC<PlaylistProps> = ({ image, name, countTracks }) => {
+    const [hover, setHover] = useState(false);
 
     const { t } = useTranslation('other')
     
     return (
         <ListItem disablePadding>
-            <ListItemButton sx={{
-                gap: 1,
-                transition: 'background-color 0.3s ease',
-                '&:hover': {
-                    backgroundColor: '#C7C7D3',
-                },
-                padding: "22px 12px",
+            <ListItemButton 
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+                sx={{
+                    gap: 1,
+                    transition: 'color 0,3s',
+                    padding: "22px 12px",
+                    '&:hover': {
+                        color: 'var(--deep-sky-blue)', // цвет текста при наведении
+                        '& .MuiTypography-root': {
+                            color: 'var(--deep-sky-blue)', // для Typography внутри
+                        },
+                        '& .MuiListItemText-root': {
+                            color: 'var(--deep-sky-blue)', // для основного текста
+                        },
+                    },
             }}>
                 <ListItemIcon>
                     <Box component="img" src={image} />
                 </ListItemIcon>
                 <ListItemText
                     disableTypography
-                    sx={{fontWeight:400, fontSize: "16px", color: "var(--columbia-blue)"}}
+                    sx={{
+                        fontWeight:400, 
+                        fontSize: "16px", 
+                        color: hover ? "var(--deep-sky-blue)" : "var(--columbia-blue)",
+                    }}
                     primary={name}
                     secondary={
                         < Box
                             component="span"
-                            sx={{ display: 'flex', color: "var(--columbia-blue)", alignItems: 'center', mt: 0.5 }}
+                            sx={{ 
+                                display: 'flex', 
+                                color: 'inherit', // наследует цвет от Box
+                                alignItems: 'center', 
+                                mt: 0.5 ,
+                            }}
                         >
-                            <Box component="img" src={pushPinImage} />
                             <Typography
-                                ml={0.5}
                                 component="span"
                                 variant={"mainRM"}
                             >

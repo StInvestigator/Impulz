dff4c7667123819e462d631aa77436e29f6e6ce0
Murashko Vladislav <vladislavuh2000@gmail.com>
2025-09-04 13:02:15 +0300

    frontend was dockerisated

--- client/src/components/lists/AlbumColaborationPlaylistList.tsx
+++ client/src/components/lists/AlbumColaborationPlaylistList.tsx
@@ -1,43 +1,43 @@
-import AlbumAverageItem from "../items/album/AlbumAverageItem.tsx";
-import PublicPlaylistAverageItem from "../items/playlist/PublicPlaylistAverageItem.tsx";
+// import AlbumAverageItem from "../items/album/AlbumAverageItem.tsx";
+// import PublicPlaylistAverageItem from "../items/playlist/PublicPlaylistAverageItem.tsx";
 import {Box} from "@mui/material";
-import ColaborationAverageItem from "../items/colaboration/ColaborationAverageItem.tsx";
+// import ColaborationAverageItem from "../items/colaboration/ColaborationAverageItem.tsx";
 
 
-const albums = [
-    "Автор 1",
-    "Автор 2",
-    "Автор 3",
-    "Автор 4",
-    "Автор 5",
-    "Автор 1",
-    "Автор 2",
-    "Автор 3",
-    "Автор 4",
-    "Автор 5",
-    "Автор 1",
-    "Автор 2",
-    "Автор 3",
-    "Автор 4",
-    "Автор 5",
-    "Автор 1",
-    "Автор 2",
-    "Автор 3",
-    "Автор 4",
-    "Автор 5",
-    "Автор 1",
-    "Автор 2",
-    "Автор 3",
-    "Автор 4",
-    "Автор 5",
-]
+// const albums = [
+//     "Автор 1",
+//     "Автор 2",
+//     "Автор 3",
+//     "Автор 4",
+//     "Автор 5",
+//     "Автор 1",
+//     "Автор 2",
+//     "Автор 3",
+//     "Автор 4",
+//     "Автор 5",
+//     "Автор 1",
+//     "Автор 2",
+//     "Автор 3",
+//     "Автор 4",
+//     "Автор 5",
+//     "Автор 1",
+//     "Автор 2",
+//     "Автор 3",
+//     "Автор 4",
+//     "Автор 5",
+//     "Автор 1",
+//     "Автор 2",
+//     "Автор 3",
+//     "Автор 4",
+//     "Автор 5",
+// ]
 
 const AlbumColaborationPlaylistList = () => {
     return (
         <Box display={"grid"} sx={{
             gridTemplateColumns: "repeat(5, 1fr)"
         }} gap={3}>
-            {albums.map((album, index) =>
+            {/* {albums.map((album, index) =>
                 index % 3 === 0
                 ?
                 <AlbumAverageItem key={index} album={album} itemHeight={360}/>
@@ -46,7 +46,7 @@ const AlbumColaborationPlaylistList = () => {
                     <PublicPlaylistAverageItem key={index} playlist={album} itemHeight={360}/>
                     :
                     <ColaborationAverageItem key={index} colaboration={album} itemHeight={360}/>
-            )}
+            )} */}
         </Box>
     );
 };
--- client/src/components/lists/AuthorList.tsx
+++ client/src/components/lists/AuthorList.tsx
@@ -1,19 +1,20 @@
-import {Box} from "@mui/material";
-import AuthorAverageItem from "../items/author/AuthorAverageItem.tsx";
-import type {FC} from "react";
+import { Box } from "@mui/material";
+// import AuthorAverageItem from "../items/author/AuthorAverageItem.tsx";
+import type { FC } from "react";
 
 interface AlbumListProps {
     authors: string[]
 }
 
-const AuthorList:FC<AlbumListProps> = ({authors}) => {
+const AuthorList: FC<AlbumListProps> = ({ authors }) => {
 
     return (
         <Box display={"grid"} sx={{
             gridTemplateColumns: "repeat(5, 1fr)"
         }} gap={3}>
-            {authors.map((author, index) =>
-                <AuthorAverageItem key={index} author={author}/>
+            {authors.map((author) =>
+                author
+                // <AuthorAverageItem key={index} author={author}/>
             )}
         </Box>
     );
